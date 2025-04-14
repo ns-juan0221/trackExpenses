ef6fe529eaba24c79238b6ad42abe102917c5ea6
N.Shion <ns-juan0221@users.noreply.github.com>
2025-04-14 16:01:38 +0900

    細かな修正

--- public/js/incomeForm.js
+++ public/js/incomeForm.js
@@ -1,25 +1,32 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const errorTexts = {
+        categoryRequired: 'カテゴリを選択してください',
+        amountRequired: '金額を入力してください',
+        amountInvalid: '金額は1以上の半角整数で入力してください',
+    };
+
     const form = document.querySelector(".needs-validation");
     const inputAmount = document.querySelector('#inputAmount');
     const amountErrorJs = document.querySelector('.amountValidationErrorJs');
     const amountError = document.querySelector('.amountValidationError');
     const inputCategory = document.querySelector('#inputCategory');
-    const categoryErrorJs = document.querySelector('.categoryValidationErrorJs');
+    const categoryErrorJs = document.querySelector('.categoryValidationErrorJS');
 
     inputAmount.addEventListener('input', () => {
         if(amountError && amountError.textContent !== ''){
             amountError.textContent = '';
         }
+
         const inputValue = inputAmount.value.trim();
 
         if (inputValue === '') {
             inputAmount.classList.remove('is-valid');
             inputAmount.classList.add('is-invalid');
-            amountErrorJs.textContent = '金額を入力してください';
+            amountErrorJs.textContent = errorTexts.amountRequired;
         } else if (isNaN(inputValue) || inputValue <= 0 || !Number.isInteger(Number(fieldValue))) {
             inputAmount.classList.remove('is-valid');
             inputAmount.classList.add('is-invalid');
-            amountErrorJs.textContent = '金額は1以上の半角整数で入力してください';
+            amountErrorJs.textContent = errorTexts.amountInvalid;
         } else {
             inputAmount.classList.remove('is-invalid');
             inputAmount.classList.add('is-valid');
@@ -31,7 +38,7 @@ document.addEventListener('DOMContentLoaded', function() {
         if (inputCategory.value === 'default' || inputCategory.value.trim() === '') {
             inputCategory.classList.remove('is-valid');
             inputCategory.classList.add('is-invalid');
-            categoryErrorJs.textContent = 'カテゴリを選択してください';
+            categoryErrorJs.textContent = errorTexts.categoryRequired;
         } else {
             inputCategory.classList.remove('is-invalid');
             inputCategory.classList.add('is-valid');
